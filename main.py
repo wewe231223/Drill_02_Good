3fb7534acb3f80d7d0f58db08f98baab97f03c90
wewe231223 <wewe231223@kpu.ac.kr>
2023-09-20 17:58:02 +0900

    원 그리기 완성

--- main.py
+++ main.py
@@ -8,14 +8,19 @@ ch = load_image("character.png")
 gr = load_image("grass.png")
 
 def run_circle():
+
     print("circle")
 
-    clear_canvas_now()
-    gr.draw_now(400,30)
-    ch.draw_now(400,90)
-    delay(1)
 
-    pass
+    cx, cy, r =  800 / 2, 600 / 2, 200
+    for deg in range(0, 360, 5):
+        x = cy + r * math.cos(math.degrees(deg))
+        y = cy + r * math.sin(math.degrees(deg))
+        clear_canvas_now()
+        gr.draw_now(400, 30)
+        ch.draw_now(x, y)
+        delay(0.01)
+
 
 def run_rectangle():
     print("rectangle")
