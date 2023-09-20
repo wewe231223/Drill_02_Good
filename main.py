1aeb54b26063f87a87d1fc2eaa8a7660eafbeb8b
wewe231223 <wewe231223@kpu.ac.kr>
2023-09-20 18:13:17 +0900

    render_all 함수 생성

--- main.py
+++ main.py
@@ -7,27 +7,36 @@ open_canvas()
 ch = load_image("character.png")
 gr = load_image("grass.png")
 
+
+def render_all(x = int,y = int):
+    clear_canvas_now()
+    gr.draw_now(400, 30)
+    ch.draw_now(x, y)
+    delay(0.01)
+
+
+
+
 def run_circle():
     print("circle")
     cx, cy, r =  400, 300, 200
     for deg in range(0, 360, 5):
         x = cx + r * math.cos(math.degrees(deg))
         y = cy + r * math.sin(math.degrees(deg))
-        clear_canvas_now()
-        gr.draw_now(400, 30)
-        ch.draw_now(x, y)
-        delay(0.1)
+        render_all(x,y)
 
 
 def run_rectangle():
 
     #bottom line
     for x in range(50, 750+1, 10):
-    
-        clear_canvas_now()
-        gr.draw_now(400, 30)
-        ch.draw_now(x, 90)
-        delay(0.1)
+        render_all(x,90)
+        pass
+
+
+
+    for x in range(750, 50, -10):
+        render_all(x,90)
         pass
 
     print("rectangle")
@@ -36,7 +45,7 @@ def run_rectangle():
 
 
 while True:
-    #run_circle()
+    run_circle()
     run_rectangle()
 
 
