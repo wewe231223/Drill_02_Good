ae8b1f7feed445256f58a542c024151d6b2057b0
wewe231223 <wewe231223@kpu.ac.kr>
2023-09-20 17:59:27 +0900

    오류수정

--- main.py
+++ main.py
@@ -12,9 +12,10 @@ def run_circle():
     print("circle")
 
 
-    cx, cy, r =  800 / 2, 600 / 2, 200
+    cx, cy, r =  400, 300, 200
+
     for deg in range(0, 360, 5):
-        x = cy + r * math.cos(math.degrees(deg))
+        x = cx + r * math.cos(math.degrees(deg))
         y = cy + r * math.sin(math.degrees(deg))
         clear_canvas_now()
         gr.draw_now(400, 30)
